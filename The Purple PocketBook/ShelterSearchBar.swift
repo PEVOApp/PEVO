0a188934b3177cf0c5b2488c040a712e38c618e4
fineblkwoman <iMe@mb-pro-2.local>
2015-11-28 11:27:48 -0500

    Fixing and adding new search to Shelter Search view controller

---
+++ The Purple PocketBook/ShelterSearchBar.swift
@@ -0,0 +1,30 @@
+//
+//  ShelterSearchBar.swift
+//  Pevo
+//
+//  Created by Alicia MacBook Pro on 11/23/15.
+//  Copyright © 2015 Alicia MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ShelterSearchBar: UISearchBar {
+    
+    var preferredFont: UIFont!
+    
+    var preferredTextColor: UIColor!
+
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        
+        // Find the index of the search filed in the search bar subviews.
+        
+
+    }
+
+
+}
--- The Purple PocketBook/ShelterSearchViewController.swift
+++ The Purple PocketBook/ShelterSearchViewController.swift
@@ -2,32 +2,44 @@
 //  ShelterSearchViewController.swift
 //  Pevo
 //
-//  Created by Alicia MacBook Pro on 10/23/15.
+//  Created by Alicia MacBook Pro on 11/23/15.
 //  Copyright © 2015 Alicia MacBook Pro. All rights reserved.
 //
 
 import UIKit
 
+protocol ShelterSearchControllerDelegate {
 
-class ShelterSearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
-    
-    @IBOutlet var searchTableView: UITableView!
-    
-    var sheltersearch:ShelterList!
-    
-    var sheltersArray = [ShelterList]()
-    var filteredShelters = [ShelterList]()
+}
 
+class ShelterSearchViewController: UITableViewController {
     
+    var searchController:UISearchController!
+    var shelterCities:[Cities] = []
+    var searchResults:[Cities] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        searchController = UISearchController(searchResultsController: nil)
+        tableView.tableHeaderView = searchController.searchBar
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func filterContentForSearchText(searchText:String){
+        searchResults = shelterCities.filter({ (city:Cities) ->
+            Bool in
+            let nameMatch = city.cityName.rangeOfString(searchText,
+                options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return nameMatch != nil
+        })
     }
     
 
